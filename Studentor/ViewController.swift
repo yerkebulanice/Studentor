79a36919a8cd732fcc48a27e0b8ba8eb01333c3e
Kapitan <erkebulanice@gmail.com>
2021-04-28 18:11:22 +0500

    Initial Commit

---
+++ Studentor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Studentor
+//
+//  Created by Еркебулан on 28.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
