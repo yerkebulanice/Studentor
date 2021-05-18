1d055af957997e08509a9dd94b946020eb84deca
Kapitan <erkebulanice@gmail.com>
2021-05-18 13:11:36 +0500

    changed some ui elements

---
+++ Studentor/Cell/GeneralQuestionsCell.swift
@@ -0,0 +1,23 @@
+//
+//  GeneralQuestionsCell.swift
+//  Studentor
+//
+//  Created by Еркебулан on 17.05.2021.
+//
+
+import UIKit
+
+class GeneralQuestionsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
